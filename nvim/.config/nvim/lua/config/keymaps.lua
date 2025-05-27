5fda047b3cce5bd49977cdcaa7faedd444952a16
Randy Carnahan <randy.carnahan@charter.com>
2025-05-27 11:48:07 -0600

    Use my options and custom keybinds.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Use <space>y to copy lines to the MacOS clipboard.
+vim.keymap.set({ "v" }, "<C-c>", '"+y')
+vim.keymap.set({ "v" }, "<C-x>", '"+d')
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- OPTIONS
+local set = vim.opt
+
+--line nums
+set.relativenumber = true
+set.number = true
+
+-- indentation and tabs
+set.tabstop = 2
+set.shiftwidth = 2
+set.autoindent = true
+set.expandtab = true
+
+-- 72nd and 80th column
+set.colorcolumn = "72,80"
+
+-- split windows
+set.splitbelow = true
+set.splitright = true
