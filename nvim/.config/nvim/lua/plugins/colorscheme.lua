84dbd23090ea65efa9b6b70baa7d1451007df069
Randy Carnahan <randy.carnahan@charter.com>
2025-05-27 11:43:15 -0600

    neovim: use new-railscast color scheme.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
-local function enable_transparency()
-  --  print('enable_transparency called.')
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+-- if true then return {} end
 
+local function enable_transparency()
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
@@ -8,21 +10,22 @@ end
 
 return {
   {
-    "lukelex/railscasts.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
+    "carakan/new-railscasts-theme",
+    -- "lukelex/railscasts.nvim",
+    -- dependencies = { "rktjmp/lush.nvim" },
 
     config = function()
-      vim.cmd.colorscheme("railscasts")
+      vim.cmd.colorscheme("new-railscasts")
 
       vim.cmd("hi Directory guibg=NONE")
       vim.cmd("hi SignColumn guibg=NONE")
 
       vim.cmd([[
-             highlight Normal guibg=none
-             highlight NonText guibg=none
-             highlight Normal ctermbg=none
-             highlight NonText ctermbg=none
-           ]])
+        highlight normal guibg=none
+        highlight nontext guibg=none
+        highlight normal ctermbg=none
+        highlight nontext ctermbg=none
+      ]])
 
       enable_transparency()
     end,
