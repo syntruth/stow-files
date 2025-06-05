990f516cf6b0150740945106f0d05cb3b8adfc9b
Randy Carnahan <randy.carnahan@charter.com>
2025-06-05 11:10:35 -0600

    Neovim colorschemes updated

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,62 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
--- if true then return {} end
-
-local function enable_transparency()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-end
-
 return {
   {
-    "carakan/new-railscasts-theme",
-    -- "lukelex/railscasts.nvim",
-    -- dependencies = { "rktjmp/lush.nvim" },
+    -- "carakan/new-railscasts-theme",
+    "lukelex/railscasts.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
 
     config = function()
-      vim.cmd.colorscheme("new-railscasts")
+      vim.cmd.colorscheme("railscasts")
 
       vim.cmd("hi Directory guibg=NONE")
       vim.cmd("hi SignColumn guibg=NONE")
 
-      vim.cmd([[
-        highlight normal guibg=none
-        highlight nontext guibg=none
-        highlight normal ctermbg=none
-        highlight nontext ctermbg=none
-      ]])
-
-      enable_transparency()
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
     end,
+  },
+
+  {
+    "everviolet/nvim",
+    name = "evergarden",
+    priority = 1000,
+    opts = {
+      theme = {
+        variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+        accent = "green",
+      },
+
+      editor = {
+        transparent_background = true,
+
+        sign = { color = "none" },
+
+        float = {
+          color = "mantle",
+          invert_border = false,
+        },
+
+        completion = {
+          color = "surface0",
+        },
+      },
+    },
+  },
+
+  {
+    "xero/miasma.nvim",
+    lazy = false,
+  },
+
+  {
+    "savq/melange-nvim",
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+
+    opts = {
+      transparent = true,
+    },
   },
 }
